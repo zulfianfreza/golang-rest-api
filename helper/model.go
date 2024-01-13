44bfbc7c6a43de84fe54f46463c2a021d419fb4b
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2024-01-13 19:25:39 +0700

    feat: category service implementation

---
+++ helper/model.go
@@ -0,0 +1,22 @@
+package helper
+
+import (
+	"github.com/zulfianfreza/golang-rest-api/model/domain"
+	"github.com/zulfianfreza/golang-rest-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
+
+func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
+	var categoryResponses []web.CategoryResponse
+	for _, category := range categories {
+		categoryResponses = append(categoryResponses, ToCategoryResponse(category))
+	}
+
+	return categoryResponses
+}
